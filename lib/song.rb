d6923fcca719c2b80eb08c6cafd81b2c36b514d8
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 04:04:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,14 +18,6 @@ class Song
     @@songs
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   self.all.count
-  # end
-
   def artist=(artist)
     @artist = artist
   end
