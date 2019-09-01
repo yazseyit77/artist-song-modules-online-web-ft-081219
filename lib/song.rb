bbd66d664932df6709b86dbc6faea9906929671f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 05:17:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,9 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
